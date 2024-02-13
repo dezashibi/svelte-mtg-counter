081e96915c5196189f7b32da1ccfb04448af2550
Navid Dezashibi <dezashibi@gmail.com>
2024-02-13 13:38:29 +0400

    components setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,18 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+    import Team from "./components/Team.svelte";
+    import { TeamType } from "./helpers/enums";
+
+    let blueScore = 20;
+    let redScore = 20;
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<div class="row mt-2">
+    <div class="col">
+        <h1 class="text-center">MTG Counter</h1>
+    </div>
+</div>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<dov class="row">
+    <Team team="Red" bind:score={redScore} teamType={TeamType.TeamA} />
+    <Team team="Blue" bind:score={blueScore} teamType={TeamType.TeamB} />
+</dov>
---
+++ src/components/Team.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import { TeamType } from "../helpers/enums";
+
+    export let team: string;
+    export let score: number;
+    export let teamType: TeamType;
+
+    function add() {
+        score += 1;
+    }
+
+    function sub() {
+        score -= 1;
+    }
+</script>
+
+<div style="border: dashed {team.toLowerCase()} 5px" class="col">
+    <h2
+        class:text-danger={teamType === TeamType.TeamA}
+        class:text-primary={teamType === TeamType.TeamB}
+    >
+        Team {team}: {score}
+    </h2>
+    <button on:click={add} class="btn btn-success">+</button>
+    <button on:click={sub} class="btn btn-danger">-</button>
+</div>
+
+<style>
+    div.col {
+        padding: 20px;
+        text-align: center;
+    }
+</style>
