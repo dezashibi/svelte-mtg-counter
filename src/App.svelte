8de68b47aebec71badf34c5943dee31c6c0549bc
Navid Dezashibi <dezashibi@gmail.com>
2024-02-13 13:52:39 +0400

    win message

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import NewReset from "./components/NewReset.svelte";
     import Team from "./components/Team.svelte";
     import { TeamType } from "./helpers/enums";
 
@@ -8,6 +9,8 @@
     $: redWon = blueScore === 0;
     $: blueWon = redScore === 0;
     $: gameOver = redWon || blueWon;
+
+    $: winningText = gameOver ? (redWon ? "Red Won" : "Blue Won") : "";
 
     function newGame() {
         blueScore = 20;
@@ -36,6 +39,14 @@
     />
 </div>
 
+<div class="row">
+    <div class="col">
+        <h2>{winningText}</h2>
+    </div>
+</div>
+
 <div class="row mt-3">
-    <button on:click={newGame} class="btn btn-warning">Reset Game</button>
+    <div class="col">
+        <NewReset {gameOver} onClick={newGame} />
+    </div>
 </div>
---
+++ src/components/NewReset.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let gameOver: boolean;
+    export let onClick: () => void;
+</script>
+
+{#if !gameOver}
+    <button on:click={onClick} class="btn btn-warning w-100">
+        Reset Game
+    </button>
+{:else}
+    <button on:click={onClick} class="btn btn-success w-100"> New Game </button>
+{/if}
